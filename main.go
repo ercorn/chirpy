7d50b404dfd346112f24d714529de301f6cb0f74
rcorn013 <rctexvn@gmail.com>
2025-10-27 15:23:23 -0400

    added and registered metric counter http handler

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileServerHits.Add(1)
+	return next
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	response := fmt.Sprint("Hits: ", cfg.fileServerHits.Load())
+	w.Write([]byte(response))
+}
 
 func main() {
+	apiCfg := apiConfig{}
 	serve_mux := http.NewServeMux()
-	serve_mux.Handle("/", http.FileServer(http.Dir(".")))
+	serve_mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	serve_mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+	serve_mux.HandleFunc("/metrics", apiCfg.metricsHandler)
 
 	server := http.Server{
 		Handler: serve_mux,
